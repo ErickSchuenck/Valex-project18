402e2137f5bb6bf76f926319b377a38acdfa4608
ErickSchuenck <esfmokdeci@gmail.com>
2022-07-10 00:21:42 -0300

    middleware aprimorado

--- src/middlewares/registerCardMiddleware.ts
+++ src/middlewares/registerCardMiddleware.ts
@@ -1,7 +1,8 @@
 import {connection} from '../config/database.js'
 import { Request, Response, NextFunction } from "express";
 import {cardSchema} from '../schemas/schemas.js'
-import {findByApiKey} from '../repositories/companyRepository.js'
+import {findByApiKey } from '../repositories/companyRepository.js'
+import {findById} from '../repositories/employeeRepository.js'
 
 export async function checkIfCardTypeIsValid(req : Request, res : Response, next : NextFunction ){
   const { error } = cardSchema.validate(req.body)
@@ -15,17 +16,27 @@ export async function checkIfCardTypeIsValid(req : Request, res : Response, next
 }
 
 export async function checkForApiExistance(req : Request, res : Response, next : NextFunction ){
-  const key : string = req.headers["x-api-key"];
-
-  console.log(key)
-
-  if (!findByApiKey(key)){ 
+  const key : string = req.headers["x-api-key"].toString();
+  const verification = await findByApiKey(key)
+  if (!verification){ 
     console.log('Api key not found in database')
     throw { 
       type: "unprocessable_entity", 
       message: "invalid api key" 
     }
   }
+  next()
+}
 
+export async function checkForWorkerExistance(req : Request, res : Response, next : NextFunction ){
+  const { workerIdentifier } : { workerIdentifier: number} = req.body;
+  const verification = await findById(workerIdentifier)
+  if (!verification){ 
+    console.log('Worker not found in database')
+    throw { 
+      type: "unprocessable_entity", 
+      message: "Worker not found in database" 
+    }
+  }
   next()
 }
--- src/routers/creationRouter.ts
+++ src/routers/creationRouter.ts
@@ -1,10 +1,24 @@
-import { Router } from "express";
-import { createCreditCard } from "../controllers/creationController.js";
-import {checkIfCardTypeIsValid, checkForApiExistance} from "../middlewares/cardMiddleware.js"
+import { 
+  Router 
+} from "express";
+import { 
+  createCreditCard 
+} from "../controllers/creationController.js";
+import {
+  checkIfCardTypeIsValid, 
+  checkForApiExistance, 
+  checkForWorkerExistance
+} from "../middlewares/registerCardMiddleware.js"
 
 
 const creationRouter = Router();
 
-creationRouter.post("/createCreditCard", checkIfCardTypeIsValid, checkForApiExistance, createCreditCard);
+creationRouter.post(
+  "/createCreditCard", 
+  checkIfCardTypeIsValid, 
+  checkForApiExistance, 
+  checkForWorkerExistance, 
+  createCreditCard
+  );
 
 export default creationRouter;
