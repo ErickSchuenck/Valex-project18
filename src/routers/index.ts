5fe8d3973783b170bbc570ff01c66b90f4e7685e
ErickSchuenck <esfmokdeci@gmail.com>
2022-07-11 21:40:30 -0300

    bugfixing balancerouter

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import creationRouter from "./creationRouter.js";
 import activationRouter from "./activationRouter.js";
+import balanceRouter from "./balanceRouter.js";
 
 const router = Router();
 router.use(creationRouter);
 router.use(activationRouter);
+router.use(balanceRouter);
 
 export default router;
