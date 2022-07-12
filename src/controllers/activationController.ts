498ad0a1f7f55a0d51337c5daf1076e7d1e21d13
ErickSchuenck <esfmokdeci@gmail.com>
2022-07-11 22:09:38 -0300

    função block card concluida

--- src/controllers/activationController.ts
+++ src/controllers/activationController.ts
@@ -1,6 +1,8 @@
 import chalk from "chalk";
 import { Request, Response } from "express";
 import * as cardsServices from "../services/cardServices.js"
+import { checkForCardExistance, checkForCardExpirationDate, checkForPasswordMatch, checkIfCardIsBlocked } from "../utils/cardUtils.js";
+import bcrypt from "bcrypt"
 
 
 export async function activateCard(req: Request, res: Response) {
@@ -8,4 +10,16 @@ export async function activateCard(req: Request, res: Response) {
   await cardsServices.activateCard(id, securityCode, password)
   console.log(chalk.red('Card Activated!'))
   res.sendStatus(200) 
+}
+
+
+export async function blockCard(req: Request, res: Response) {
+  const {id, password} = req.body;
+  const encryptedPassword = bcrypt.hashSync(password, 10);
+  const card = await checkForCardExistance(id);
+  await checkForPasswordMatch(id, encryptedPassword);
+  await checkForCardExpirationDate(card.expirationDate);
+  await checkIfCardIsBlocked(card);
+  await cardsServices.blockCard(id);
+  res.send('Card blocked').status(200)
 }
